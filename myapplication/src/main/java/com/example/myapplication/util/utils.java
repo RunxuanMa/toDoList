af8dbdff5a9b0e2944a2f8dfa49b293facf43f99
小马马 <1817671010@qq.com>
2022-07-07 20:36:01 +0800

    测试提交

--- myapplication/src/main/java/com/example/myapplication/util/utils.java
+++
@@ -1,9 +0,0 @@
-package com.example.myapplication.util;
-
-import android.content.Context;
-
-public class utils {
-    public static int dip2px(Context context,float dpValue){
-        return 1;
-    }
-}
